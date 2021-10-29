4a5308e2a4fd2d06d4779af33c43c386b71a5e2d
ALPpedro <pedro.henrique24239@gmail.com>
2021-10-29 16:20:14 -0300

    usando as classes

--- script.js
+++ script.js
@@ -1,3 +1,24 @@
 function dupla(eu){
     alert(eu);
 }
+
+
+// objeto
+const cleitin={
+    nome: "Pedrinho",
+    idade: 25,
+    rua: "Rua São Paulo",
+    familia:[
+    "Fabiana",
+    "Andre Ricardo",
+    "Andre Pauli",
+    "Leonardo"
+    ],
+    musicas:[
+        "Por supuesto",
+        "Eu to bem",
+        "Birds",
+        "12"
+    ]
+}
+    console.log("Seu nome é: "+cleitin.nome+" Uma de suas musicas favoritas é: "+cleitin.musicas[1]);
